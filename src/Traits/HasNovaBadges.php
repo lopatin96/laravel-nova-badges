cc87af63b2997f7c869e13a0cc34079dcb351037
vladyslavlopatin <ilopatin3@gmail.com>
2023-11-16 22:02:08 +0100

    fix where

--- src/Traits/HasNovaBadges.php
+++ src/Traits/HasNovaBadges.php
@@ -6,6 +6,6 @@ trait HasNovaBadges
 {
     public function scopeNewModel($query, int $hoursToDetect = 3)
     {
-        $query->whereDate('created_at', '>=', now()->subHours($hoursToDetect));
+        $query->where('created_at', '>=', now()->subHours($hoursToDetect));
     }
 }
